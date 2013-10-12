2f6b098497f1ca0c0e307e265c2225b9b66a3213
Oriol Romero <oriol@inoxtags.com>
2013-10-12 17:31:40 +0200

    -a

--- apps/backend_stripe/urls.py
+++ apps/backend_stripe/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 
-from apps.backend_stripe.views import CheckoutSummaryView
+from apps.backend_stripe.views import CheckoutSummaryView, ChargeView
 
 
 urlpatterns = patterns('',
     url(r'^checkout/$', login_required(CheckoutSummaryView.as_view()), name='checkout_summary_stripe'),
+    url(r'^charge/$', login_required(ChargeView.as_view()), name='stripe_charge'),
 )
--- apps/backend_stripe/views.py
+++ apps/backend_stripe/views.py
@@ -3,6 +3,7 @@
 Stripe payment views.
 """
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -18,37 +19,37 @@ class CheckoutSummaryView(TemplateView):
 
     template_name = 'stripe/checkout_summary.html'
 
-    def post(self, request, *args, **kwargs):
-        # Get the CartPrice
-        context_price = RequestContext(request).get('CartPrice')
-        price_cents = context_price * 100
-
-        # Get the credit card details submitted by the form
-        token = request.POST['stripeToken']
-
-        # Create the charge on Stripe's servers - this will charge the user's
-        # card
-        try:
-            charge = stripe.Charge.create(
-                amount=price_cents,  # amount in cents, again
-                currency="eur",
-                card=token,
-                description="payinguser@example.com"
-            )
-        except stripe.CardError, e:
-              # The card has been declined
-            pass
-
-        if charge:
-            HttpResponseRedirect('/payment/success')
-        else:
-            HttpResponseRedirect('/payment/error')
-
-        return render(request, self.template_name)
-
     def get_context_data(self, **kwargs):
         context = super(CheckoutSummaryView, self).get_context_data(**kwargs)
         context_price = RequestContext(self.request).get('CartPrice')
         context['price_cents'] = context_price * 100
         context['stripe_key'] = STRIPE_PUBLISHABLE
         return context
+
+
+class ChargeView(FormView):
+
+    success_url = '/payment/success/'
+
+    def form_valid(self, form):
+        # Get the CartPrice
+        context_price = RequestContext(self.request).get('CartPrice')
+        price_cents = context_price * 100
+
+        # Get the credit card details submitted by the form
+        token = self.request.POST['stripeToken']
+
+        # Create the charge on Stripe's servers - this will charge the user's
+        # card
+        try:
+            stripe.Charge.create(
+                amount=price_cents,  # amount in cents, again
+                currency="eur",
+                card=token,
+                description="payinguser@example.com"
+            )
+        except stripe.CardError:
+              # The card has been declined
+            pass
+
+        return super(ChargeView, self).form_valid(form)

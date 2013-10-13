082b3a2b406ba34ad6de6f4e27f975171ff37e7e
Oriol Romero <oriol@inoxtags.com>
2013-10-13 18:50:06 +0200

    -a

--- apps/backend_stripe/views.py
+++ apps/backend_stripe/views.py
@@ -7,11 +7,17 @@ from django.views.generic.edit import FormView
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from decimal import Decimal
+from django.utils.translation import ugettext_lazy as _
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
 
 import stripe
 
 from inoxtags.settings import STRIPE_SECRET, STRIPE_PUBLISHABLE
 from apps.backend_stripe.forms import StripeForm
+from apps.billing.models import Iva
 
 stripe.api_key = STRIPE_SECRET
 
@@ -58,6 +64,64 @@ class ChargeView(FormView):
                 card=token,
                 description=description
             )
+
+            # Once the charge is made, creates the order, the order items,
+            # sends the confirmation email and marks the cart as checked out.
+            iva = Iva.objects.filter(is_active=True).get()
+            count_total = cart.get_count_total()
+            user=self.request.user,
+
+            # Creates the order
+            order = Order.objects.create(
+                cart=cart,
+                user=user,
+                to=user.name,
+                address=user.shipping_address,
+                postal_code=user.shipping_code,
+                town=user.shipping_town,
+                country=user.shipping_country.country,
+                count=count_total,
+                price=context_price,
+                iva=Decimal(str(iva)),
+                payed=True
+            )
+
+            # Creates the order items
+            for item in product_list:
+                OrderItem.objects.create(
+                    order=order,
+                    quantity=item.quantity,
+                    product=item.product,
+                    color=item.color,
+                    front_main=item.front_main,
+                    front_tel=item.front_tel,
+                    back_1=item.back_1,
+                    back_2=item.back_2,
+                    back_3=item.back_3,
+                    price=item.price,
+                    price_special_1=item.price_special_1,
+                    price_special_2=item.price_special_2,
+                    price_special_3=item.price_special_3,
+                    price_special_4=item.price_special_4,
+                    price_in_hand=item.price_in_hand,
+                    made=item.made,
+                    repetition=item.repetition
+                )
+
+            # Send a confirmation email
+            subject = _("Comanda gestionada correctament a la web d'INOXtags.")
+            email = user.email
+            html_content = render_to_string('email/conf_order.html', {'order': order, 'products': product_list})
+            text_content = strip_tags(html_content)
+
+            msg = EmailMultiAlternatives(subject, text_content, to=[email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+            # Mark the cart as checked out
+            cart.checked_out = True
+            cart.save()
+
         except stripe.CardError:
               # The card has been declined
             pass

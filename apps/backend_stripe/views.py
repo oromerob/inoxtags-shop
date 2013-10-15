0869312b54622116b59b3d4422a653f56d9acc59
Oriol Romero <oriol@inoxtags.com>
2013-10-15 13:22:02 +0200

    -a

--- apps/backend_stripe/views.py
+++ apps/backend_stripe/views.py
@@ -57,7 +57,7 @@ class ChargeView(FormView):
         # Create the charge on Stripe's servers - this will charge the user's
         # card
         try:
-            stripe.Charge.create(
+            charge = stripe.Charge.create(
                 amount=price_cents,  # amount in cents, again
                 currency="eur",
                 card=token,
@@ -123,6 +123,67 @@ class ChargeView(FormView):
 
         except stripe.CardError:
               # The card has been declined
+            pass
+
+        if charge:
+
+            # Once the charge is made, creates the order, the order items,
+            # sends the confirmation email and marks the cart as checked out.
+            iva = Iva.objects.filter(is_active=True).get()
+            count_total = cart.get_count_total()
+            user = self.request.user
+
+            # Creates the order
+            order = Order.objects.create(
+                cart=cart,
+                user=user,
+                to=user.name,
+                address=user.shipping_address,
+                postal_code=user.shipping_code,
+                town=user.shipping_town,
+                country=user.shipping_country.country,
+                count=count_total,
+                price=context_price,
+                iva=Decimal(str(iva)),
+                payed=True
+            )
+
+            # Creates the order items
+            for item in product_list:
+                OrderItem.objects.create(
+                    order=order,
+                    quantity=item.quantity,
+                    product=item.product,
+                    color=item.color,
+                    front_main=item.front_main,
+                    front_tel=item.front_tel,
+                    back_1=item.back_1,
+                    back_2=item.back_2,
+                    back_3=item.back_3,
+                    price=item.price,
+                    price_special_1=item.price_special_1,
+                    price_special_2=item.price_special_2,
+                    price_special_3=item.price_special_3,
+                    price_special_4=item.price_special_4,
+                    price_in_hand=item.price_in_hand,
+                    made=item.made,
+                    repetition=item.repetition
+                )
+
+            # Send a confirmation email
+            subject = _("Comanda gestionada correctament a la web d'INOXtags.")
+            email = user.email
+            html_content = render_to_string('email/conf_order.html', {'order': order, 'products': product_list})
+            text_content = strip_tags(html_content)
+
+            msg = EmailMultiAlternatives(subject, text_content, to=[email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+            # Mark the cart as checked out
+            cart.checked_out = True
+            cart.save()
+        else:
             HttpResponseRedirect('/charge/error/')
 
         return super(ChargeView, self).form_valid(form)

7bdd8ea32436b1b2b586c5a43b4c387d7a3ecf86
Oriol Romero <oriol@inoxtags.com>
2013-10-15 13:34:03 +0200

    -a

--- apps/backend_stripe/urls.py
+++ apps/backend_stripe/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 
-from apps.backend_stripe.views import CheckoutSummaryView, ChargeView, ChargeSuccessView, ChargeErrorView
+from apps.backend_stripe.views import CheckoutSummaryView, ChargeView, ChargeResultView
 
 
 urlpatterns = patterns('',
     url(r'^checkout/$', login_required(CheckoutSummaryView.as_view()), name='checkout_summary_stripe'),
     url(r'^charge/$', login_required(ChargeView.as_view()), name='stripe_charge'),
-    url(r'^charge/success/$', login_required(ChargeSuccessView.as_view()), name='stripe_charge_success'),
-    url(r'^charge/error/$', login_required(ChargeErrorView.as_view()), name='stripe_charge_error'),
+    url(r'^charge/result/$', login_required(ChargeResultView.as_view()), name='stripe_charge_success'),
 )

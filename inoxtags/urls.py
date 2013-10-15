94cc41588415d9e67f1a6042c82c7968f7c1c0d4
Oriol Romero <oriol@inoxtags.com>
2013-10-15 19:31:13 +0200

    -a

--- inoxtags/urls.py
+++ inoxtags/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
     url(r'^find_us/', include('apps.partners.urls')),
     url(r'^staff/', include('apps.staff.urls')),
     url(r'zebra/', include('zebra.urls', namespace="zebra", app_name='zebra')),
+    url(r'^rosetta/', include('rosetta.urls')),
 )
 
 if settings.DEBUG:
